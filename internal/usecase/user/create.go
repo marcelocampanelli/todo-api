aed7ec7c510996de2af8c44aa4f6ef37f0d121d0
Marcelo Campanelli <marcelo.campanelli@yahoo.com>
2023-09-27 20:35:17 -0300

    created user create usecase

--- internal/usecase/user/create.go
+++ internal/usecase/user/create.go
@@ -1,4 +1,37 @@
 package user
 
-type CreateInputDTO struct {
+import (
+	"github.com/marcelocampanelli/todo-api/internal/domain/entity"
+	"github.com/marcelocampanelli/todo-api/internal/infra/database"
+)
+
+type CreateUserInputDTO struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserOutputDTO struct {
+	ID string `json:"id"`
+}
+
+type CreateUseCase struct {
+	ucCreate database.UserInterface
+}
+
+func NewUserCreateUseCase(ucCreate database.UserInterface) *CreateUseCase {
+	return &CreateUseCase{
+		ucCreate: ucCreate,
+	}
+}
+
+func (uc *CreateUseCase) Create(input *CreateUserInputDTO) (*CreateUserOutputDTO, error) {
+	user, err := entity.NewUser(input.Name, input.Email, input.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateUserOutputDTO{
+		ID: user.ID.String(),
+	}, nil
 }
