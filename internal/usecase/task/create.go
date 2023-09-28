925925df742ec91dcd86cfbd2ebcda034ad4bfee
Marcelo Campanelli <marcelo.campanelli@yahoo.com>
2023-09-28 16:09:35 -0300

    creatd usecase

---
+++ internal/usecase/task/create.go
@@ -0,0 +1,37 @@
+package task
+
+import (
+	"github.com/marcelocampanelli/todo-api/internal/domain/entity"
+	"time"
+)
+
+type CreateInputDTO struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Finished  bool      `json:"finished"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type CreateOutputDTO struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type CreateTaskUseCase struct {
+	TaskRepository entity.TaskRepositoryInterface
+}
+
+func NewCreateTaskUseCase(taskRepository entity.TaskRepositoryInterface) *CreateTaskUseCase {
+	return &CreateTaskUseCase{TaskRepository: taskRepository}
+}
+
+func (c *CreateTaskUseCase) Execute(input CreateInputDTO) (CreateOutputDTO, error) {
+	task, _ := entity.NewTask(input.Name, input.Finished)
+	err := c.TaskRepository.Create(task)
+	if err != nil {
+		return CreateOutputDTO{}, err
+	}
+
+	return CreateOutputDTO{ID: task.ID.String(), Name: task.Name}, nil
+}
