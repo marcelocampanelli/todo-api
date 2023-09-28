43f5e7f32ae236e63d9a2132ac366ff7732e8431
Marcelo Campanelli <marcelo.campanelli@yahoo.com>
2023-09-28 11:30:45 -0300

    fix

--- internal/infra/webserver/starter.go
+++ internal/infra/webserver/starter.go
@@ -3,9 +3,3 @@ package webserver
 type WebServerStarter struct {
 	WebServer WebServer
 }
-
-func NewWebServerStarter(webServer WebServer) *WebServerStarter {
-	return &WebServerStarter{
-		WebServer: webServer,
-	}
-}
