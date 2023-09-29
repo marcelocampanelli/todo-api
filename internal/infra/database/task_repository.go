476b1e68f3ffae241afc62efdde75d3cb09d67c1
Marcelo Campanelli <marcelo.campanelli@yahoo.com>
2023-09-29 13:47:06 -0300

    added logs in repositories

--- internal/infra/database/task_repository.go
+++ internal/infra/database/task_repository.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/marcelocampanelli/todo-api/internal/domain/entity"
 )
 
@@ -16,11 +17,13 @@ func NewTaskRepository(db *sql.DB) *TaskRepository {
 func (r *TaskRepository) Create(task *entity.Task) error {
 	stmt, err := r.Db.Prepare("INSERT INTO tasks (id, name, finished, created_at, updated_at, user_id) VALUES ($1, $2, $3, $4, $5, $6)")
 	if err != nil {
+		fmt.Println("Error in task repository, method: create, action: prepare SQL. Error: ", err)
 		return err
 	}
 
 	_, err = stmt.Exec(task.ID, task.Name, task.Finished, task.CreatedAt, task.UpdatedAt, task.UserID)
 	if err != nil {
+		fmt.Println("Error in task repository, method: create, action: exec SQL. Error: ", err)
 		return err
 	}
 
@@ -30,11 +33,13 @@ func (r *TaskRepository) Create(task *entity.Task) error {
 func (r *TaskRepository) Update(task *entity.Task) error {
 	stmt, err := r.Db.Prepare("UPDATE tasks SET name=$1, finished=$2, updated_at=$3 WHERE id=$4")
 	if err != nil {
+		fmt.Println("Error in task repository, method: update action: prepare SQL. Error: ", err)
 		return err
 	}
 
 	_, err = stmt.Exec(task.Name, task.Finished, task.UpdatedAt, task.ID)
 	if err != nil {
+		fmt.Println("Error in task repository, method: update action: exec SQL. Error: ", err)
 		return err
 	}
 
@@ -46,11 +51,13 @@ func (r *TaskRepository) FindById(id string) (*entity.Task, error) {
 
 	stmt, err := r.Db.Prepare("SELECT id, name, finished, updated_at FROM tasks WHERE id=$1")
 	if err != nil {
+		fmt.Println("Error in task repository, method: findById action: prepare SQL. Error: ", err)
 		return nil, err
 	}
 
 	err = stmt.QueryRow(id).Scan(&task.ID, &task.Name, &task.Finished, &task.UpdatedAt)
 	if err != nil {
+		fmt.Println("Error in task repository, method: findById action: exec SQL. Error: ", err)
 		return nil, err
 	}
 
@@ -60,11 +67,13 @@ func (r *TaskRepository) FindById(id string) (*entity.Task, error) {
 func (r *TaskRepository) Delete(task *entity.Task) error {
 	stmt, err := r.Db.Prepare("DELETE FROM tasks WHERE id=$1")
 	if err != nil {
+		fmt.Println("Error in task repository, method: Delete action: prepare SQL. Error: ", err)
 		return err
 	}
 
 	_, err = stmt.Exec(task.ID)
 	if err != nil {
+		fmt.Println("Error in task repository, method: Delete action: exec SQL. Error: ", err)
 		return err
 	}
 
@@ -76,11 +85,14 @@ func (r *TaskRepository) FindAll(userID string) ([]*entity.Task, error) {
 
 	stmt, err := r.Db.Prepare("SELECT id, name, finished, updated_at FROM tasks WHERE user_id=$1")
 	if err != nil {
+		fmt.Println("Error in task repository, method: FindAll action: prepare SQL. Error: ", err)
 		return nil, err
 	}
 
 	rows, err := stmt.Query(userID)
 	if err != nil {
+		fmt.Println("Error in task repository, method: FindAll action: exec SQL. Error: ", err)
+
 		return nil, err
 	}
 
@@ -88,6 +100,7 @@ func (r *TaskRepository) FindAll(userID string) ([]*entity.Task, error) {
 		var task entity.Task
 		err := rows.Scan(&task.ID, &task.Name, &task.Finished, &task.UpdatedAt)
 		if err != nil {
+			fmt.Println("Error in task repository, method: FindAll action: create task collection return. Error: ", err)
 			return nil, err
 		}
 		tasks = append(tasks, &task)
--- internal/infra/database/user_repository.go
+++ internal/infra/database/user_repository.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/marcelocampanelli/todo-api/internal/domain/entity"
 )
 
@@ -16,11 +17,13 @@ func NewUserRepositoy(db *sql.DB) *UserRepository {
 func (r *UserRepository) Create(user *entity.User) error {
 	stmt, err := r.Db.Prepare("INSERT INTO users (id, name, email, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6)")
 	if err != nil {
+		fmt.Println("Error in user repository, method: Create action: prepare SQL. Error: ", err)
 		return err
 	}
 
 	_, err = stmt.Exec(user.ID, user.Name, user.Email, user.Password, user.CreatedAt, user.UpdatedAt)
 	if err != nil {
+		fmt.Println("Error in user repository, method: Create action: exec SQL. Error: ", err)
 		return err
 	}
 
@@ -30,11 +33,13 @@ func (r *UserRepository) Create(user *entity.User) error {
 func (r *UserRepository) Update(user *entity.User) error {
 	stmt, err := r.Db.Prepare("UPDATE users SET name=$1, email=$2, password=$3, updated_at=$4 WHERE id=$5")
 	if err != nil {
+		fmt.Println("Error in user repository, method: Update action: prepare SQL. Error: ", err)
 		return err
 	}
 
 	_, err = stmt.Exec(user.Name, user.Email, user.Password, user.UpdatedAt, user.ID)
 	if err != nil {
+		fmt.Println("Error in user repository, method: Update action: exec SQL. Error: ", err)
 		return err
 	}
 
@@ -46,11 +51,13 @@ func (r *UserRepository) FindById(id string) (*entity.User, error) {
 
 	stmt, err := r.Db.Prepare("SELECT id, name, email, password, updated_at FROM users WHERE id=$1")
 	if err != nil {
+		fmt.Println("Error in user repository, method: FindById action: prepare SQL. Error: ", err)
 		return nil, err
 	}
 
 	err = stmt.QueryRow(id).Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.UpdatedAt)
 	if err != nil {
+		fmt.Println("Error in user repository, method: FindById action: exec SQL. Error: ", err)
 		return nil, err
 	}
 
